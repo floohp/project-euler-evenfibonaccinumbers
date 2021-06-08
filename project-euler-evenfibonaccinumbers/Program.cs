537a95e8887598e15d946156abd9e52594db5a23
Floohp <Floohp@DESKTOP-S85HFGV>
2021-06-08 11:51:47 +0200

    Add project files.

---
+++ project-euler-evenfibonaccinumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace project_euler_evenfibonaccinumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 2;
+            int a = 1;
+            int b = 2;
+
+            while (b < 3000000)
+            {
+                int tempA = a;
+                a = b;
+                b = tempA + b;
+                if (b % 2 == 0)
+                    total = total + b;
+                Console.WriteLine("Fibonacci: " + b);
+                Console.WriteLine("Total of even: " + total);
+            }
+            Console.ReadLine();
+        }
+    }
+}
